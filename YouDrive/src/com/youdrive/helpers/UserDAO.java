1fb329b22d7a7330cf40568693e86cb6108df610
Jane Ullah <janeullah@gmail.com>
2013-11-17 12:46:07 -0500

    Checking in changes.

--- YouDrive/src/com/youdrive/helpers/UserDAO.java
+++ YouDrive/src/com/youdrive/helpers/UserDAO.java
@@ -18,6 +18,10 @@ public class UserDAO implements IUserManager {
 	private PreparedStatement addAdminStmt;
 	private PreparedStatement addUserStmt;
 	private PreparedStatement deleteUserByUsernameStmt;
+	private PreparedStatement addMembershipStmt;
+	private PreparedStatement deleteMembershipStmt;
+	private PreparedStatement updateMembershipStmt;
+
 	private Constants cs = new Constants();
 	private Connection conn = null;
 	private static LocationDAO ldao = null;
@@ -31,6 +35,8 @@ public class UserDAO implements IUserManager {
 			addAdminStmt = conn.prepareStatement("insert into " + Constants.USERS + " values (DEFAULT,?,?,?,?,DEFAULT,DEFAULT,?,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,?,DEFAULT,DEFAULT)",Statement.RETURN_GENERATED_KEYS);
 			addUserStmt = conn.prepareStatement("insert into " + Constants.USERS + " values (DEFAULT,?,?,?,?,?,?,?,?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
 			deleteUserByUsernameStmt = conn.prepareStatement("select * from " + Constants.USERS + " where username = ?");
+			addMembershipStmt = conn.prepareStatement("insert into " + Constants.MEMBERSHIP + " values (DEFAULT,?,?,?)" ,Statement.RETURN_GENERATED_KEYS);
+			deleteMembershipStmt = conn.prepareStatement("delete from " + Constants.MEMBERSHIP + " where id = ?"); 
 			System.out.println("Instantiated LocationDAO");
 		}catch(SQLException e){
 			System.err.println(e.getErrorCode());
@@ -48,7 +54,7 @@ public class UserDAO implements IUserManager {
 			addAdminStmt.setString(3, firstName);
 			addAdminStmt.setString(4, lastName);
 			addAdminStmt.setString(5, email);
-			userID = addAdminStmt.executeUpdate();
+			addAdminStmt.executeUpdate();
 			ResultSet rs = addAdminStmt.getGeneratedKeys();
 			if (rs.next()){
 				userID = rs.getInt(1);
@@ -56,7 +62,7 @@ public class UserDAO implements IUserManager {
 		}catch(SQLException e){
 			System.err.println(cs.getError(e.getErrorCode()));
 		}catch(Exception e){
-			System.err.println("Problem with addVehicle method: " + e.getClass().getName() + ": " + e.getMessage());			
+			System.err.println("Problem with addAdmin method: " + e.getClass().getName() + ": " + e.getMessage());			
 		}
 		return userID;
 	}
@@ -76,9 +82,8 @@ public class UserDAO implements IUserManager {
 			addAdminStmt.setInt(9, p.getCcNumber());
 			addAdminStmt.setInt(10, p.getCcSecurityCode());
 			addAdminStmt.setDate(11, p.getCcExpirationDate());
-			addAdminStmt.setInt(12, p.get);
-			addAdminStmt.setBoolean(13, p.isAdmin());
-			addAdminStmt.setDate(12, p.getMemberExpiration());
+			addAdminStmt.setBoolean(12, p.isAdmin());
+			addAdminStmt.setDate(13, p.getMemberExpiration());
 			userID = addAdminStmt.executeUpdate();
 			ResultSet rs = addAdminStmt.getGeneratedKeys();
 			if (rs.next()){
@@ -87,8 +92,44 @@ public class UserDAO implements IUserManager {
 		}catch(SQLException e){
 			System.err.println(cs.getError(e.getErrorCode()));
 		}catch(Exception e){
-			System.err.println("Problem with addVehicle method: " + e.getClass().getName() + ": " + e.getMessage());			
+			System.err.println("Problem with addUser method: " + e.getClass().getName() + ": " + e.getMessage());			
 		}
 		return userID;
+	}
+
+	@Override
+	public int addMembership(String name, double price, int duration) {
+		int membershipID = 0;
+		try{
+			addMembershipStmt.setString(1, name);
+			addMembershipStmt.setDouble(2, price);
+			addMembershipStmt.setInt(3, duration);
+			addMembershipStmt.executeUpdate();
+			ResultSet rs = addMembershipStmt.getGeneratedKeys();
+			if (rs.next()){
+				membershipID = rs.getInt(1);
+			}
+		}catch(SQLException e){
+			System.err.println(cs.getError(e.getErrorCode()));
+		}catch(Exception e){
+			System.err.println("Problem with addMembership method: " + e.getClass().getName() + ": " + e.getMessage());			
+		}
+		return membershipID;
+	}
+
+	@Override
+	public String deleteMembership(int id) {
+		String errorCode = "";
+		try{
+			deleteMembershipStmt.setInt(1, id);
+			deleteMembershipStmt.executeUpdate();
+		}catch(SQLException e){
+			errorCode = String.valueOf(e.getErrorCode());
+			System.err.println(cs.getError(e.getErrorCode()));
+		}catch(Exception e){
+			errorCode = "Error";
+			System.err.println("Problem with addMembership method: " + e.getClass().getName() + ": " + e.getMessage());			
+		}
+		return errorCode;
 	}
 }
--- YouDrive/src/com/youdrive/interfaces/IUserManager.java
+++ YouDrive/src/com/youdrive/interfaces/IUserManager.java
@@ -5,4 +5,6 @@ import com.youdrive.models.User;
 public interface IUserManager {
 	public int addAdmin(String username, String password, String firstName, String lastName, String email);
 	public int addUser(User p);
+	public int addMembership(String name, double price, int duration);
+	public String deleteMembership(int id);
 }
